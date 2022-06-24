998ba7c7e9ae75e35112901219bb8ad51eebcf9e
kereminci <54918783+kereminci@users.noreply.github.com>
2022-06-24 17:41:22 +0300

    ci/cd (#3)
    * testing
    
    * added ci/cd -Game Activity Service : Create microservice skeleton with Nodejs#208
    
    * minor changes -Game Activity Service: Create microservice skeleton with Nodejs #208

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,18 +2,37 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { EventEmitterModule } from '@nestjs/event-emitter';
+import { UserStatusService } from 'src/user-status/services/user-status.service';
+import { UserStatusRepo } from 'src/user-status/repos/user-status.repo';
+import { mockDeep } from 'jest-mock-extended'
+import { UserStatusController } from 'src/user-status/user-status.controller';
+
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
+  let userStatusRepoInstance;
 
   beforeEach(async () => {
+    userStatusRepoInstance = mockDeep<UserStatusRepo>();
+
+    let userStatusRepoProvider = {
+      provide: UserStatusRepo,
+      useValue: userStatusRepoInstance
+    };
+
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [AppModule, EventEmitterModule],
+      providers: [UserStatusService, userStatusRepoProvider],
+      controllers: [UserStatusController],
+      
     }).compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
   });
+
+  
 
   it('/ (GET)', () => {
     return request(app.getHttpServer())
@@ -21,4 +40,6 @@ describe('AppController (e2e)', () => {
       .expect(200)
       .expect('Hello World!');
   });
+
+
 });
